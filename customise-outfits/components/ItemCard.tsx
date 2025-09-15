de5cc2e2b5dcf73ba2aadee249760e8cd79c9011
Sakshi Tilkari <122168229+SakshiTilkari@users.noreply.github.com>
2025-09-15 17:23:52 +0530

    Update ItemCard.tsx

--- customise-outfits/components/ItemCard.tsx
+++ customise-outfits/components/ItemCard.tsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
   cardImage: {
     width: '100%',
     height: 150,
-    resizeMode: 'cover',
+     resizeMode: 'contain',
   },
   cardBody: {
     padding: 10,
@@ -46,4 +46,5 @@ const styles = StyleSheet.create({
   },
   badgeText: { fontSize: 12, color: '#555' },
 });
+
 
